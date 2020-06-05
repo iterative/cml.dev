9120fa8a2558aa4d6b6ecf05dd166f3d9cb08961
rogermparent <rogermparent@gmail.com>
2020-06-05 16:21:50 -0400

    Move homepage imports to global shortcodes

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,40 +1,85 @@
 import React, { useContext, useEffect, useRef } from "react"
 import { ModeContext } from "components/organisms/SwitchableMode/Provider"
-import { Box } from "@theme-ui/components"
+import { Button, Box, Container, Link } from "@theme-ui/components"
+import { JSONTabs } from "components/organisms/Tabs"
 
-export default {
-  Video: ({
-    sx = {},
-    mode,
-    autoplay,
-    playsinline=true,
-    controls=true,
-    ...props
-  }) => {
-    const contextMode = useContext(ModeContext)
-    const videoRef = useRef()
-    const videoElement = videoRef.current
-    useEffect(() => {
-      if (mode) {
-        if (contextMode !== mode && videoElement) {
-          videoElement.pause()
+import Switchable from "components/organisms/SwitchableMode/Switchable"
+import Switch from "components/organisms/SwitchableMode/Switch"
+
+const Tooltip = ({
+  sx = {},
+  as = "span",
+  className,
+  contents,
+  children
+}) => {
+  return (
+    <Box
+    as={as}
+    variant="styles.Tooltip"
+    className={className}
+    sx={{
+      "& aside": {
+        display: "hidden"
+      },
+      "&:focus": {
+        "& aside": {
+          display: "block"
         }
       }
-    }, [mode, contextMode, videoElement])
-    return (
-      <Box
-        as="video"
-        ref={videoRef}
-        autoplay={autoplay}
-        playsinline={playsinline}
-        controls={controls}
-        sx={{
-          my: 4,
-          maxWidth: "100%",
-          ...sx,
-        }}
-        {...props}
-      />
-    )
-  },
+    }}
+    >
+    {children}
+    <aside>{contents}</aside>
+  </Box>
+  )
+}
+
+const Video = ({
+  sx = {},
+  mode,
+  autoplay,
+  playsinline=true,
+  controls=true,
+  ...props
+}) => {
+  const contextMode = useContext(ModeContext)
+  const videoRef = useRef()
+  const videoElement = videoRef.current
+  useEffect(() => {
+    if (mode) {
+      if (contextMode !== mode && videoElement) {
+        videoElement.pause()
+      }
+    }
+  }, [mode, contextMode, videoElement])
+  return (
+    <Box
+      as="video"
+      ref={videoRef}
+      autoplay={autoplay}
+      playsinline={playsinline}
+      controls={controls}
+      sx={{
+        my: 4,
+        maxWidth: "100%",
+        ...sx,
+      }}
+      {...props}
+    />
+  )
+}
+
+export default {
+  Tooltip,
+  Video,
+  JSONTabs,
+
+  Switchable,
+  Switch,
+
+  Button,
+  Box,
+  Container,
+  Link
 }
