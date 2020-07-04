a42cd25b22016af19cd43bed73da363e8f53ef84
rogermparent <rogermparent@gmail.com>
2020-07-04 00:01:47 -0400

    Change title pipe to dash
    Just my preference, really

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require(`dotenv`).config()
 
 module.exports = {
   siteMetadata: {
-    title: `CML | CI for Machine Learning`,
+    title: `CML - CI for Machine Learning`,
     description: `CML is continuous integration for machine learning. Bring DevOps to data science.`,
     author: `Iterative`,
     siteUrl: process.env.DEPLOY_URL || process.env.URL || "https://cml.dev",
