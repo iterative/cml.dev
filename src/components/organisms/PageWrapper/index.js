b93c897ebaa0a9a6f1acddd0656e6e68fc8ee086
charnysh-dev <dzianischarnysh1121@gmail.com>
2021-06-10 13:42:48 -0400

    chore: without provider

--- src/components/organisms/PageWrapper/index.js
+++ src/components/organisms/PageWrapper/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import Page from '../Page'
-import { ThemeProvider } from 'theme-ui'
-import { Theme } from '@theme-ui/css'
-import theme from '../../../gatsby-plugin-theme-ui'
 
 export default function PageWrapper({ element, props }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Page {...props}>{element}</Page>
-    </ThemeProvider>
-  )
+  return <Page {...props}>{element}</Page>
 }
