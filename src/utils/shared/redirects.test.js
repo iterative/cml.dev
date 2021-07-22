aa5fe84dd9de4782207233b7777caa19975ff1fd
charnysh-dev <dzianischarnysh1121@gmail.com>
2021-07-22 12:18:47 -0400

    chore: recover redirects of dvc

--- src/utils/shared/redirects.test.js
+++ src/utils/shared/redirects.test.js
@@ -1,4 +1,9 @@
-const { buildSidebarRedirects, processRedirectString } = require('./redirects')
+const url = require('url')
+const {
+  buildSidebarRedirects,
+  processRedirectString,
+  getRedirect
+} = require('./redirects')
 
 describe('buildRedirectsList', () => {
   it('builds correct redirects list', () => {
@@ -43,5 +48,56 @@ describe('processRedirectString', () => {
 
     const { matchPathname } = processRedirectString('^/path /y')
     expect(matchPathname).toEqual(true)
+  })
+})
+
+describe('getRedirects', () => {
+  it('enforces HTTPS and removes www simultaneously', () => {
+    const mockReq = {
+      headers: {
+        'x-forwarded-proto': 'http'
+      },
+      url: '/foo/bar?baz'
+    }
+    expect(
+      getRedirect('www.dvc.org', '/not-used', { req: mockReq, dev: false })
+    ).toEqual([301, 'https://dvc.org/foo/bar?baz'])
+  })
+
+  const itRedirects = (source, target, code = 301) => {
+    const addHost = pathOrUrl => {
+      if (pathOrUrl.startsWith('/')) {
+        return `https://dvc.org${pathOrUrl}`
+      }
+      return pathOrUrl
+    }
+
+    it(`${source} -> ${target} (${code})`, () => {
+      source = addHost(source)
+      const { hostname, pathname } = url.parse(source)
+      const [rCode, rLocation] = getRedirect(hostname, pathname)
+
+      expect(rLocation).toEqual(target)
+      expect(rCode).toEqual(code)
+
+      // Detect redirect loops.
+      const secondUrl = url.parse(addHost(rLocation))
+      const secondRedirect = getRedirect(secondUrl.hostname, secondUrl.pathname)
+
+      // allow second redirect only if it removes trailing slash
+      if (secondRedirect.length) {
+        const thirdUrl = url.parse(addHost(secondRedirect[1]))
+        expect(secondUrl.host).toEqual(thirdUrl.host)
+        expect(secondUrl.pathname.replace(/\/$/, '')).toEqual(secondRedirect[1])
+
+        const thirdRedirect = getRedirect(thirdUrl.hostname, thirdUrl.pathname)
+        expect(thirdRedirect).toEqual([])
+      }
+    })
+  }
+
+  describe('fromPaths', () => {
+    itRedirects('/docs/x', '/doc/x')
+    itRedirects('/documentation/x', '/doc/x')
   })
 })
