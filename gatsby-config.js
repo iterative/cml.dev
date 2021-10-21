4245623f4e2e3781581e71e9ee252492cb81c21f
julieg18 <juliannagal18@gmail.com>
2021-10-21 17:43:08 -0500

    Update head links * add favicon.ico * add 180x180 icon to head

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,7 +120,54 @@ const plugins = [
       name: 'CML',
       short_name: 'CML',
       start_url: '/',
-      theme_color: '#663399'
+      theme_color: '#663399',
+      icons: [
+        {
+          src: 'icons/icon-48x48.png',
+          sizes: '48x48',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-72x72.png',
+          sizes: '72x72',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-96x96.png',
+          sizes: '96x96',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-180x180.png',
+          sizes: '180x180',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-256x256.png',
+          sizes: '256x256',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
       /* eslint-enable @typescript-eslint/camelcase */
     }
   },
