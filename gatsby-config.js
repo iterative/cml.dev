511dac2761974dd1b2968d3f6ceb491cf236c0d9
julieg18 <juliannagal18@gmail.com>
2021-10-22 06:32:16 -0500

    Update gatsby-plugin-manifest * move icons to root directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,47 +123,47 @@ const plugins = [
       theme_color: '#663399',
       icons: [
         {
-          src: 'icons/icon-48x48.png',
+          src: '/icon-48x48.png',
           sizes: '48x48',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-72x72.png',
+          src: '/icon-72x72.png',
           sizes: '72x72',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-96x96.png',
+          src: '/icon-96x96.png',
           sizes: '96x96',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-144x144.png',
+          src: '/icon-144x144.png',
           sizes: '144x144',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-180x180.png',
+          src: '/icon-180x180.png',
           sizes: '180x180',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-192x192.png',
+          src: '/icon-192x192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-256x256.png',
+          src: '/icon-256x256.png',
           sizes: '256x256',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-384x384.png',
+          src: '/icon-384x384.png',
           sizes: '384x384',
           type: 'image/png'
         },
         {
-          src: 'icons/icon-512x512.png',
+          src: '/icon-512x512.png',
           sizes: '512x512',
           type: 'image/png'
         }
