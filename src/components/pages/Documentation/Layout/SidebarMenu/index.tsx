03e41ac35736abb3a25a2f4c8ae13600a2d8699e
Thomas Kunwar <yathomasi@gmail.com>
2022-03-15 14:13:45 +0545

    sidenav fix, docs color fix

--- src/components/pages/Documentation/Layout/SidebarMenu/index.tsx
+++ src/components/pages/Documentation/Layout/SidebarMenu/index.tsx
@@ -98,6 +98,12 @@ const SidebarMenuItem: React.FC<ISidebarMenuItemProps> = ({
     isLeafItem && styles.sidebarLeafBullet
   )
 
+  const bulletIconJSX = isLeafItem ? (
+    <span className={bulletIconClassName}></span>
+  ) : (
+    <button onClick={bulletIconClick} className={bulletIconClassName}></button>
+  )
+
   const parentElement =
     type === 'external' ? (
       <Link
@@ -121,17 +127,7 @@ const SidebarMenuItem: React.FC<ISidebarMenuItemProps> = ({
         className={className}
         onClick={currentLevelOnClick}
       >
-        {iconElement ? (
-          iconElement
-        ) : (
-          <span
-            className={bulletIconClassName}
-            onClick={bulletIconClick}
-            onKeyDown={bulletIconClick}
-            role="button"
-            tabIndex={0}
-          ></span>
-        )}
+        {iconElement ? iconElement : bulletIconJSX}
         {label}
       </Link>
     )
