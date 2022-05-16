7b28d010c43f518f0f6e26624af57fc0eda6fa8f
Thomas Kunwar <yathomasi@gmail.com>
2022-05-16 17:00:06 +0545

    removed unused markdownContent

--- src/gatsby/models.js
+++ src/gatsby/models.js
@@ -1,8 +1,7 @@
-const markdownContent = require('./models/markdown-content')
 const imageSourcePaths = require('./models/image-source-paths')
 const github = require('./models/github')
 const pruneCache = require('./models/prune-cache')
 
-const models = [markdownContent, imageSourcePaths, github, pruneCache]
+const models = [imageSourcePaths, github, pruneCache]
 
 module.exports = models
