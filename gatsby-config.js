8fae2f796cb45ac459afe636782cc9cdb8634df1
Thomas Kunwar <yathomasi@gmail.com>
2022-09-23 13:38:57 +0545

    Revert "Bump theme to 0.1.16 and update Sentry integration (#326)"
    This reverts commit e983a10c277e77a12b8ca0ff83f242cde8501c0e.

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,7 +106,29 @@ const plugins = [
       ]
     }
   },
-  '@sentry/gatsby'
+  {
+    resolve: 'gatsby-plugin-sentry',
+    options: {
+      dsn: process.env.SENTRY_DSN,
+      environment: process.env.NODE_ENV,
+      release: process.env.SOURCE_VERSION,
+      enabled: process.env.NODE_ENV === 'production',
+      ignoreErrors: [
+        /* When we deploy new version we delete assets which were generated for
+        the previous deployed version, but users can have opened old version in 
+        their browsers. If they hover some link on the page Gatsby.js will try
+        fetch old chunks and will get ChunkLoadError, but then will load static
+        page from the new deployed version and all will be ok. So we can just
+        ignore these type of errors */
+        'ChunkLoadError'
+      ],
+      /* There are some common urls which recomment to ignore. It's even 
+      mentioned in the official documentation: https://docs.sentry.io/platforms/javascript/#decluttering-sentry
+      In our case we just ignore all errors from the browser's extensions,
+      because we can't influence on then somehow. */
+      blacklistUrls: [/extensions\//i, /^chrome:\/\//i]
+    }
+  }
 ]
 
 if (process.env.GITHUB_TOKEN) {
