0185fa55610ea420c3cbad13af25b4be95704899
Thomas Kunwar <yathomasi@gmail.com>
2022-11-30 14:11:02 +0545

    update not found page

--- src/components/pages/NotFound/index.tsx
+++ src/components/pages/NotFound/index.tsx
@@ -1,14 +1,18 @@
+import MainLayout from '@dvcorg/gatsby-theme-iterative/src/components/MainLayout'
+import { PageProps } from 'gatsby'
 import React from 'react'
 
 import * as styles from './styles.module.css'
 
-const NotFound: React.FC = () => (
-  <div className={styles.wrapper}>
-    <h1 className={styles.title}>Not Found</h1>
-    <div className={styles.content}>
-      You just hit a route that doesn&#39;t exist... the sadness.
+const NotFound = ({ location }: PageProps) => (
+  <MainLayout location={location}>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Not Found</h1>
+      <div className={styles.content}>
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </div>
     </div>
-  </div>
+  </MainLayout>
 )
 
 export default NotFound
