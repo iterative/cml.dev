9b19868e7407157ede2feeb72498f7f83da11040
Thomas Kunwar <yathomasi@gmail.com>
2022-12-01 23:28:43 +0545

    update for seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import SEO from '@dvcorg/gatsby-theme-iterative/src/components/SEO'
 import React from 'react'
 
 import Home from '../components/pages/Home'
 
-const HomePage: React.FC = () => <Home />
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <SEO />
+      <Home />
+    </>
+  )
+}
 
 export default HomePage
